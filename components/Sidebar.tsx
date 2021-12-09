210618e8839896bba49b39c363643fdf38446e88
rajeshmurugaiyan <rajesh.sam94@gmail.com>
2021-12-09 07:08:36 +0100

    initial contribution

---
+++ components/Sidebar.tsx
@@ -0,0 +1,20 @@
+function Sidebar() {
+    return (
+        <div>
+            <img 
+                src="https://raw.githubusercontent.com/rajeshmurugaiyan/linkinbio/master/rajesh/images/pic.jpg" 
+                alt="user avatar"
+            />
+            <h3>
+                <span>
+                Rajesh 
+                </span>
+                MOUROUGAYEN
+            </h3>
+            <p>Web Developer</p>
+            <p>Download Resume</p>
+        </div>
+    )
+}
+
+export default Sidebar
